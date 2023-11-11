384b4e3d447e41c123a22b85511711a8111ddabf
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-11-11 00:09:13 -0300

    feat: add models Category and Product

---
+++ ProductsAPI/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductsAPI.Models;
+
+public class Category
+{
+    public int CategoryId { get; set; }
+    public string? Name { get; set; }
+    public string? ImageUrl { get; set; }
+}
+
