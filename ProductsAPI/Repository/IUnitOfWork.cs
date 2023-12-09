aed4957200d9814f5694288756378967edbf415f
Lucas Eduardo Ferreira da Rosa <lucas.eferreira@totvs.com.br>
2023-12-09 13:58:42 -0300

    feat: add Unit of Work pattern

---
+++ ProductsAPI/Repository/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductsAPI.Repository;
+
+public interface IUnitOfWork
+{
+    // UnitOfWork will be responsible for creating the repositories
+    IProductRepository ProductRepository { get; }
+    ICategoryRepository CategoryRepository { get; }
+    void Commit();
+}
---
+++ ProductsAPI/Repository/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using ProductsAPI.Context;
+
+namespace ProductsAPI.Repository;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private ProductRepository _productRepository;
+    private ICategoryRepository _categoryRepository;
+    public AppDbContext _context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // Implementing the IUnitOfWork interface
+    public IProductRepository ProductRepository => _productRepository ?? new ProductRepository(_context);
+    public ICategoryRepository CategoryRepository => _categoryRepository ?? new CategoryRepository(_context);
+
+    // Save pending changes to the data store
+    public void Commit() => _context.SaveChanges();
+
+    public void Dispose() => _context.Dispose();
+}
